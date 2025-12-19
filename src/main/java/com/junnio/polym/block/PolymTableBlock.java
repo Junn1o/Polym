9f2e2b2bdcb512d7da9f324c95e6b29789ff87d0
Junnio <nphan218@gmail.com>
2025-12-20 01:47:12 +0700

    updated to 1.21.10

--- src/main/java/com/junnio/polym/block/PolymTableBlock.java
+++ src/main/java/com/junnio/polym/block/PolymTableBlock.java
@@ -5,8 +5,6 @@ import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.CraftingInventory;
-import net.minecraft.inventory.CraftingResultInventory;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
@@ -28,9 +26,6 @@ public class PolymTableBlock extends Block {
             player.openHandledScreen(new NamedScreenHandlerFactory() {
                 @Override
                 public ScreenHandler createMenu(int syncId, PlayerInventory inv, PlayerEntity player) {
-                    // Create the crafting and result inventories for this screen handler
-                    CraftingInventory craftingInventory = new CraftingInventory(null, 3, 3); // 3x3 crafting grid
-                    CraftingResultInventory resultInventory = new CraftingResultInventory();
                     return new PolymTableScreenHandler(syncId, inv);
                 }
 
